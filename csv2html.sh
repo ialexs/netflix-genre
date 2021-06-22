167d1810395cbe0c9aeff096ca61f087661780b9
ilya /nrd /x1 <ialexs@narrada.com>
2021-06-22 19:07:44 +0700

    pandoc url kolorr gon

--- csv2html.sh
+++ csv2html.sh
@@ -7,7 +7,7 @@ rm -rf $file.html $file.md $file.pdf
 # Visidata - https://visidata.org
 vd $file.csv -b -o $file.html
 
-sed 's/https:\/\/netflix.com\/browse\/genre\/.*[0-9]/<a href="&" target="_blank">&<\/a>/g' -i $file.html
+sed 's/https:\/\/www.netflix.com\/browse\/genre\/.*[0-9]/<a href="&" target="_blank">&<\/a>/g' -i $file.html
 sed 's/-genre<\/h2>/-genre (some might not available in your region) - <a href=\"'$file.pdf'\">pdf<\/a><\/h2>/g' -i $file.html
 
 # Prettier - https://prettier.io
