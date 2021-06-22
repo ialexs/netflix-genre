8c52ccf8d34402f28a7389bab3b85545443d4908
ilya /nrd /x1 <ialexs@narrada.com>
2021-06-22 20:15:50 +0700

    yha

--- csv2html.sh
+++ csv2html.sh
@@ -8,12 +8,12 @@ rm -rf $file.html $file.md $file.pdf
 vd $file.csv -b -o $file.html
 
 sed 's/https:\/\/www.netflix.com\/browse\/genre\/.*[0-9]/<a href="&" target="_blank">&<\/a>/g' -i $file.html
-sed 's/-genre<\/h2>/-genre (some might not available in your region) - <a href=\"'$file.pdf'\">pdf<\/a><\/h2>/g' -i $file.html
+sed 's/-genre<\/h2>/-genre (some might not available in your region) - <a href=\"'$file.pdf'\">PDF<\/a><\/h2>/g' -i $file.html
 
 # Prettier - https://prettier.io
 prettier -w $file.html
 
 # Pandoc - https://pandoc.org
-pandoc -V urlcolor=red -V geometry:a4paper,margin=1cm --pdf-engine=xelatex $file.html -o $file.pdf
+pandoc -V urlcolor=blue -V geometry:a4paper,landscape,margin=.5cm --pdf-engine=pdflatex $file.html -o $file.pdf
 
-cp $file.html index.html
+# cp $file.html index.html
